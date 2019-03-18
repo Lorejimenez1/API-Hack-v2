f63d7a516ce00abad0811b0da2c31e1a1101d86d
Lorenzo Jimenez <lorenzojimenez101@yahoo.com>
2019-03-18 12:51:36 -0700

    establish ui and functionality for new stocks table

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
 const CRYPTO_SEARCH_URL= 'https://cors-anywhere.herokuapp.com/'+'https://api.bitfinex.com/v2/tickers';
 const CRYPTO_NEWS_URL= 'https://newsapi.org/v2/everything';
+const bitfinex_api = 'https://cors-anywhere.herokuapp.com/'+'https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD,tMKRUSD,tBABUSD,tETHUSD,tDSHUSD,tBSVUSD,tLTCUSD,tZECUSD,tXMRUSD,tDGXUSD,tREPUSD,tMLNUSD';
 
+
+function getAllStocks() {
+  $.getJSON(bitfinex_api, displayAllStocks);
+}
+
+function displayAllStocks(data) {
+  
+  let stockArray = data;
+
+  $('#js-ticker-all-results').html(` 
+  <table class="darkTable">
+  <thead>
+    <tr>
+    <th>Symbol</th>
+    <th>Last Price</th>
+    <th>24H Change</th>
+    <th>24H High</th>
+    <th>24H Low</th>
+    <th>24H Volume</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td>${stockArray[0][0].slice(1, 4)}</td><td>${stockArray[0][1]}</td><td>${stockArray[0][2].toFixed(2)}</td><td>${stockArray[0][3]}</td><td>${stockArray[0][4].toFixed(2)}</td><td>${stockArray[0][5]}</td></tr>
+    <tr>
+    <td>${stockArray[1][0].slice(1, 4)}</td><td>${stockArray[1][1]}</td><td>${stockArray[1][2].toFixed(2)}</td><td>${stockArray[1][3]}</td><td>${stockArray[1][4].toFixed(2)}</td><td>${stockArray[1][5]}</td></tr>
+    <tr>
+    <td>${stockArray[2][0].slice(1, 4)}</td><td>${stockArray[2][1]}</td><td>${stockArray[2][2].toFixed(2)}</td><td>${stockArray[2][3]}</td><td>${stockArray[2][4].toFixed(2)}</td><td>${stockArray[2][5]}</td></tr>
+    <tr>
+    <td>${stockArray[3][0].slice(1, 4)}</td><td>${stockArray[3][1]}</td><td>${stockArray[3][2].toFixed(2)}</td><td>${stockArray[3][3]}</td><td>${stockArray[3][4].toFixed(2)}</td><td>${stockArray[3][5]}</td></tr>
+    <tr>
+    <td>${stockArray[4][0].slice(1, 4)}</td><td>${stockArray[4][1]}</td><td>${stockArray[4][2].toFixed(2)}</td><td>${stockArray[4][3]}</td><td>${stockArray[4][4].toFixed(2)}</td><td>${stockArray[4][5]}</td></tr>
+    <tr>
+    <td>${stockArray[5][0].slice(1, 4)}</td><td>${stockArray[5][1]}</td><td>${stockArray[5][2].toFixed(2)}</td><td>${stockArray[5][3]}</td><td>${stockArray[5][4].toFixed(2)}</td><td>${stockArray[5][5]}</td></tr>
+    <tr>
+    <td>${stockArray[6][0].slice(1, 4)}</td><td>${stockArray[6][1]}</td><td>${stockArray[6][2].toFixed(2)}</td><td>${stockArray[6][3]}</td><td>${stockArray[6][4].toFixed(2)}</td><td>${stockArray[6][5]}</td></tr>
+    <tr>
+    <td>${stockArray[7][0].slice(1, 4)}</td><td>${stockArray[7][1]}</td><td>${stockArray[7][2].toFixed(2)}</td><td>${stockArray[7][3]}</td><td>${stockArray[7][4].toFixed(2)}</td><td>${stockArray[7][5]}</td></tr>
+    <tr>
+    <td>${stockArray[8][0].slice(1, 4)}</td><td>${stockArray[8][1]}</td><td>${stockArray[8][2].toFixed(2)}</td><td>${stockArray[8][3]}</td><td>${stockArray[8][4].toFixed(2)}</td><td>${stockArray[8][5]}</td></tr>
+    <tr>
+    <td>${stockArray[9][0].slice(1, 4)}</td><td>${stockArray[9][1]}</td><td>${stockArray[9][2].toFixed(2)}</td><td>${stockArray[9][3]}</td><td>${stockArray[9][4].toFixed(2)}</td><td>${stockArray[9][5]}</td></tr>
+    <tr>
+    <td>${stockArray[10][0].slice(1, 4)}</td><td>${stockArray[10][1]}</td><td>${stockArray[10][2].toFixed(2)}</td><td>${stockArray[10][3]}</td><td>${stockArray[10][4].toFixed(2)}</td><td>${stockArray[10][5]}</td></tr>
+    </tbody>
+    </tr>
+  </table>
+  `);
+}
 function getDataFromNewsApi(searchTerm, searchTermTwo, callback) {
   const query = {
   q: `"${searchTerm}","${searchTermTwo}","Crypto"`,
@@ -37,9 +87,9 @@ function renderNews(result) {
   	`;
 }
  
-function getDataFromBitfinex(searchTerm, searchTermTwo, callback) {
+function getDataFromBitfinex(searchTerm, callback) {
   const query = {
-  symbols: `t${searchTerm}USD,t${searchTermTwo}USD`
+  symbols: `t${searchTerm}USD,t${searchTerm}BTC`
   
 }
   $.getJSON(CRYPTO_SEARCH_URL, query, callback);
@@ -52,65 +102,53 @@ function displaySearchData(data) {
 }
 
 function renderResult(result) {
-  //console.log(result)
-  const myArray = result
+   //console.log(result)
+  let stockArray = result
+  if (result.length > 1) {
   return `
-  <div id="grid-1" class="col-6">
-    <table border="2">
+  <table class="darkTable">
+  <thead>
+    <tr>
+    <th>Symbol</th>
+    <th>Last Price</th>
+    <th>24H Change</th>
+    <th>24H High</th>
+    <th>24H Low</th>
+    <th>24H Volume</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td>${stockArray[0][0].slice(1)}</td><td>${stockArray[0][1]}</td><td>${stockArray[0][2].toFixed(2)}</td><td>${stockArray[0][3]}</td><td>${stockArray[0][4].toFixed(2)}</td><td>${stockArray[0][5]}</td></tr>
+    <tr>
+    <td>${stockArray[1][0].slice(1)}</td><td>${stockArray[1][1]}</td><td>${stockArray[1][2].toFixed(2)}</td><td>${stockArray[1][3]}</td><td>${stockArray[1][4].toFixed(2)}</td><td>${stockArray[1][5]}</td></tr>
+    </tbody>
+    </tr>
+  </table>
+    `;
+  }
+  else {
+    return `
+    <table class="darkTable">
+    <thead>
       <tr>
-				<td>Symbol</td>
-        <td>${myArray[0][0]}</td>
-      </tr>      
-      <tr>
-        <td>Price(usd)</td>              
-        <td>$${myArray[0][7]}</td>
+      <th>Symbol</th>
+      <th>Last Price</th>
+      <th>24H Change</th>
+      <th>24H High</th>
+      <th>24H Low</th>
+      <th>24H Volume</th>
       </tr>
+      </thead>
+      <tbody>
       <tr>
-        <td>DailyChange(perc.)</td>
-        <td>%${myArray[0][6]}</td>
-      </tr>          
+      <td>${stockArray[0][0].slice(1)}</td><td>${stockArray[0][1]}</td><td>${stockArray[0][2].toFixed(2)}</td><td>${stockArray[0][3]}</td><td>${stockArray[0][4].toFixed(2)}</td><td>${stockArray[0][5]}</td></tr>
       <tr>
-        <td>VOLUME24HR</td>
-        <td>${myArray[0][8]}</td>
+      </tbody>
       </tr>
-      <tr>
-      	<td>Low of Day</td>
-        <td>${myArray[0][10]}</td>
-      </tr>
-      <tr>          
-      	<td>High Of Day</td>
-      	<td>${myArray[0][9]}</td>
-      </tr>
-        </table>    
-  </div>
-  <div id="grid-1" class="col-6">
-    <table  border="2">
-      <tr>
-				<td>Symbol</td>
-        <td>${myArray[1][0]}</td>
-      </tr>      
-      <tr>
-        <td>Price(usd)</td>              
-        <td>$${myArray[1][7]}</td>
-      </tr>
-      <tr>
-        <td>DailyChange(perc.)</td>
-        <td>%${myArray[1][6]}</td>
-      </tr>          
-      <tr>
-        <td>VOLUME24HR</td>
-        <td>${myArray[1][8]}</td>
-      </tr>
-      <tr>
-      	<td>Low of Day</td>
-        <td>${myArray[1][10]}</td>
-      </tr>
-      <tr>          
-      	<td>High Of Day</td>
-      	<td>${myArray[1][9]}</td>
-      </tr>
-    </table>    
-  </div>`;
+    </table>
+      `;
+  }
 }
 
 function watchSubmit() {
@@ -118,19 +156,37 @@ function watchSubmit() {
 		event.preventDefault();
 		const queryTarget = $(event.currentTarget).find('#js-query');
 		const query = queryTarget.val().toUpperCase() ;
-		const queryVs = $(event.currentTarget).find('#js-query-2');
-		const queryTwo = queryVs.val().toUpperCase();
-    getDataFromBitfinex(query, queryTwo, displaySearchData);
-    getDataFromNewsApi(query, queryTwo, displayNewsData);
+    getDataFromBitfinex(query, displaySearchData);
+    getDataFromNewsApi(query, displayNewsData);
     });
 }
 function watchCompareButton() {
-  $(".nav-button").click(function(event) {
+  $(".nav-compare").click(function(event) {
     event.stopPropagation();
-    $(".col-4").empty();
-    $(".col-12").css("visibility", "visible");
+    b = $(".col-4").detach();    
+    $('#js-ticker-all-results').prop('hidden', false);
+    $("table").css("visibility", "visible");
+  });
+}
+function home() {
+  $(".nav-home").click(function(event) {
+    event.stopPropagation();
+    $("#landing-section").prepend(x);
+    $("#landing-section").prepend(b);
+    $("table").css("visibility", "hidden");
+  });
+}
+function watchTickersButton() {
+  $(".nav-tickers").click(function(event) {
+    event.stopPropagation();
+    x = $(".col-4").detach();     
+    $("#js-search-form").css("display", "block");
   });
 }
 
+
 $(watchCompareButton);
+$(watchTickersButton);
 $(watchSubmit);
+$(home);
+$(getAllStocks);
